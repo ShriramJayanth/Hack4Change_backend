1adf27a25abf520c83d89668b543e7190a0f8581
ShriramJayanth <csshriramjayanth.cse2022@citchennai.net>
2024-08-01 20:49:53 +0530

    cors completely removed

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ dotenv.config();
 const app:Express=express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 // app.use(cors({
 //   credentials:true,
 //   origin:["http://localhost:3000"],
