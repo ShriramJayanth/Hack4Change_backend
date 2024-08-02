51bb021869b6e002a54eab99475a1d7bcaee7e68
ShriramJayanth <csshriramjayanth.cse2022@citchennai.net>
2024-08-02 16:33:42 +0530

    mani san wa baka desu

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,10 @@ const app: Express = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: 'https://hack4-change-front-end.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+app.use(cors());
 
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
-
-app.use("/auth", cors(corsOptions), authRoutes);
-app.use("/code", cors(corsOptions), codeRoutes);
+app.use("/auth", authRoutes);
+app.use("/code", codeRoutes);
 
 const port = process.env.PORT || 3002;
 
