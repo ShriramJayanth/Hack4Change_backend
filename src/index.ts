c19f2c82fc9add5ab25c3524301edecee4498d3b
ShriramJayanth <csshriramjayanth.cse2022@citchennai.net>
2024-08-02 16:23:25 +0530

    tried new method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express,{ Express,Request,Response } from "express";
+import express, { Express, Request, Response } from "express";
 import authRoutes from "./routes/auth";
 import codeRoutes from "./routes/code";
 import cookieParser from "cookie-parser";
@@ -7,26 +7,26 @@ import cors from "cors";
 
 dotenv.config();
 
-const app:Express=express();
+const app: Express = express();
 app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'https://hack4-change-front-end.vercel.app',
-    optionsSuccessStatus: 200,
-  };
-  
+  origin: 'https://hack4-change-front-end.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
-// app.use(cors());
+app.use("/auth", authRoutes);
+app.use("/code", codeRoutes);
 
+const port = process.env.PORT || 3002;
 
-app.use("/auth",authRoutes);
-app.use("/code",codeRoutes);
-
-const port=process.env.PORT || 3002;
-
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
